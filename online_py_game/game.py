3539d61480dc1ea3ef27dce3132dd39679bdbf2d
Felix Leonte <leontefelix@yahoo.com>
2020-12-22 18:52:35 +0100

    made the basic client and server make a new game

---
+++ online_py_game/game.py
@@ -0,0 +1,26 @@
+class Game:
+    def __init__(self, id):
+        self.p1Went = False
+        self.p2Went = False
+        self.ready = False
+        self.id = id
+        self.moves = [None, None]
+        self.wins = [0,0]
+        self.ties = 0
+
+    def get_player_move(self, p):
+        """
+        :param p: [0,1]
+        return: Move
+        """
+        return self.moves[p]
+
+    def player(self, player, move):
+        self.moves[player] = move
+        if player == 0:
+            self.p1Went = True
+        else:
+            self.p2Went = True
+
+
+
